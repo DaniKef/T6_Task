e20342eadc3266ec47ca945a6ba68bf142a92a0f
DaniilH <gurenko888@gmail.com>
2021-10-30 15:53:56 +0300

    variantA commit

--- VariantA/FirstProgram.cs
+++ VariantA/FirstProgram.cs
@@ -33,7 +33,7 @@ namespace VariantA
                 indexCount--;
             }
         }
-        public IEnumerable Count(int max)
+        public IEnumerable Count(int max) //
         {
             for (int i = 0; i < max; i++)
             {
